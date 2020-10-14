c4f91cb5f47dcbcbe603977a01f90ce12deaa72a
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-10-13 21:19:47 -0300

    Add simple memory reader api calls; Add test name reader

---
+++ MediviaTaskReader/APICall/Memory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MediviaTaskReader.APICall
+{
+  public static class Memory
+  {
+    [DllImport("kernel32.dll")]
+    static extern bool ReadProcessMemory(IntPtr hProcess,
+    UIntPtr lpBaseAddress, [Out] byte[] lpBuffer, uint dwSize, out IntPtr lpNumberOfBytesRead);
+
+    public static byte[] ReadBytes(IntPtr handle, ulong address, uint bytesToRead)
+    {
+      IntPtr ptrBytesRead;
+      byte[] buffer = new byte[bytesToRead];
+
+      ReadProcessMemory(handle, new UIntPtr(address), buffer, bytesToRead, out ptrBytesRead);
+
+      return buffer;
+    }
+    public static byte ReadByte(IntPtr handle, ulong address)
+    {
+      return ReadBytes(handle, address, 1)[0];
+    }
+    public static string ReadString(IntPtr handle, ulong address)
+    {
+      string s = "";
+      byte temp = ReadByte(handle, address++);
+      while (temp != 0)
+      {
+        s += (char)temp;
+        temp = ReadByte(handle, address++);
+      }
+      return s;
+    }
+
+    public static Int32 ReadInt32(IntPtr handle, ulong address)
+    {
+      return BitConverter.ToInt32(ReadBytes(handle, address, 4), 0);
+    }
+
+    public static UInt64 ReadUInt64(IntPtr handle, ulong address)
+    {
+      return BitConverter.ToUInt64(ReadBytes(handle, address, 8), 0);
+    }
+  }
+}
--- MediviaTaskReader/Form1.cs
+++ MediviaTaskReader/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using MediviaTaskReader.APICall;
 
 namespace MediviaTaskReader
 {
@@ -15,6 +17,24 @@ namespace MediviaTaskReader
     public Form1()
     {
       InitializeComponent();
+    }
+
+    private void Button1_Click(object sender, EventArgs e)
+    {
+      Process test = Process.GetProcessesByName("Medivia")[0];
+      UInt64 baseAddress = Convert.ToUInt64(test.MainModule.BaseAddress.ToInt64());
+      UInt64 nameBaseAddress = baseAddress + 0xD43950;
+      UInt64 nameLengthAddress = Memory.ReadUInt64(test.Handle, nameBaseAddress) + 0x40;
+      Int32 nameLength = Memory.ReadInt32(test.Handle, nameLengthAddress);
+      if(nameLength > 16)
+      {
+        UInt64 nameAddress = Memory.ReadUInt64(test.Handle, Memory.ReadUInt64(test.Handle, nameBaseAddress) + 0x30);
+        MessageBox.Show(Memory.ReadString(test.Handle, nameAddress));
+      } else
+      {
+
+      }
+      MessageBox.Show(Memory.ReadInt32(test.Handle, nameLengthAddress).ToString());
     }
   }
 }
