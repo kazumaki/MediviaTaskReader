8b10a449a38a2e183e90aff01503acd3c3b695fd
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-10-13 21:57:23 -0300

    Add character object to handle character name reading

--- MediviaTaskReader/Form1.cs
+++ MediviaTaskReader/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using MediviaTaskReader.APICall;
+using MediviaTaskReader.Objects;
 
 namespace MediviaTaskReader
 {
@@ -21,20 +21,9 @@ namespace MediviaTaskReader
 
     private void Button1_Click(object sender, EventArgs e)
     {
-      Process test = Process.GetProcessesByName("Medivia")[0];
-      UInt64 baseAddress = Convert.ToUInt64(test.MainModule.BaseAddress.ToInt64());
-      UInt64 nameBaseAddress = baseAddress + 0xD43950;
-      UInt64 nameLengthAddress = Memory.ReadUInt64(test.Handle, nameBaseAddress) + 0x40;
-      Int32 nameLength = Memory.ReadInt32(test.Handle, nameLengthAddress);
-      if(nameLength > 16)
-      {
-        UInt64 nameAddress = Memory.ReadUInt64(test.Handle, Memory.ReadUInt64(test.Handle, nameBaseAddress) + 0x30);
-        MessageBox.Show(Memory.ReadString(test.Handle, nameAddress));
-      } else
-      {
-
-      }
-      MessageBox.Show(Memory.ReadInt32(test.Handle, nameLengthAddress).ToString());
+      Process test = Process.GetProcessesByName("Medivia")[1];
+      Character testChar = new Character(test);
+      MessageBox.Show(testChar.Name());
     }
   }
 }
---
+++ MediviaTaskReader/Objects/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using MediviaTaskReader.APICall;
+using System.Windows.Forms;
+
+namespace MediviaTaskReader.Objects
+{
+  public class Character
+  {
+    const Byte nameLengthOffset = 0x40;
+    const Byte nameOffset = 0x30;
+    const Int32 nameBaseOffset = 0xD43950;
+
+    private IntPtr handle;
+    private UInt64 baseAddress;
+    public Character(Process process)
+    {
+      this.handle = process.Handle;
+      this.baseAddress = Convert.ToUInt64(process.MainModule.BaseAddress.ToInt64());
+    }
+
+    public string Name()
+    {
+      UInt64 localBase = Memory.ReadUInt64(this.handle, this.baseAddress + nameBaseOffset);
+      Int32 nameLength = Memory.ReadInt32(this.handle, localBase + nameLengthOffset);
+      MessageBox.Show(nameLength.ToString());
+      if(nameLength > 16)
+      {
+        UInt64 nameAddress = Memory.ReadUInt64(this.handle, localBase + nameOffset);
+        return Memory.ReadString(this.handle, nameAddress);
+      }
+
+      return Memory.ReadString(this.handle, localBase + nameOffset);
+    }
+
+  }
+}
